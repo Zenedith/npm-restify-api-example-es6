a53bca031367ec307db4c75f2338fa1b14ead78c
Zenedith <github@zenedith.mailgun.org>
2017-03-04 22:18:52 +0100

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+var winston = require('winston');
+var minimst = require('minimist');
+var API = require('./lib/api').API;
+var UsersController = require('./lib/controllers/usersController').UsersController;
+
+winston.log('info', 'Hello distributed log files!');
+winston.info(minimst(process.argv.slice(2)));
+winston.info(winston.hash('sss'));
+
+var api = new API();
+var usersController = new UsersController(winston);
+
+var routes = [
+  {
+    path: '/users',
+    method: 'get',
+    controller: usersController.getUsers
+  }
+];
+
+api.run(process.env.PORT || 3000, routes);
